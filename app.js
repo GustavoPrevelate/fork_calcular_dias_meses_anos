4678913b30763c1756a49d2190cb36e04dd8f2d7
GuPTeste <prevelategame.2020@gmail.com>
2024-04-29 08:50:04 -0300

    meu segundo commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function calculateDifference() {
+    var date1 = moment(document.getElementById('date1').value);
+    var date2 = moment(document.getElementById('date2').value);
+
+    var daysDiff = date2.diff(date1, 'days');
+    var weeksDiff = date2.diff(date1, 'weeks');
+    var monthsDiff = date2.diff(date1, 'months');
+    var yearsDiff = date2.diff(date1, 'years');
+
+    document.getElementById('days').innerText = daysDiff;
+    document.getElementById('weeks').innerText = weeksDiff;
+    document.getElementById('months').innerText = monthsDiff;
+    document.getElementById('years').innerText = yearsDiff;
+}
